2bbfb4499dbf2e8749b769c8737d7ef4e5ee6fd1
Kamaropoulos <kamaropoulos@outlook.com>
2019-02-07 11:55:01 +0200

    feat: Model and Controller

---
+++ api/controllers/LinksController.js
@@ -0,0 +1,48 @@
+/**
+ * LinksController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    list: function (req, res) {
+        Links.find({}).sort({'createdAt': -1}).exec(function(err, links){
+            if (err){
+                res.send(500, {error: "DB Error"});
+            }
+            res.view('list', {links:links});
+        })
+    },
+    add: function(req, res){
+        res.view('add');
+    },
+    create: function(req, res){
+        var title = req.body.title;
+        var url = req.body.url;
+
+        Links.create({title:title, url:url}).exec(function (err) {
+            if (err){
+                res.send(500, {error: 'DB Error'});
+            }
+
+            res.redirect('/links/list');
+        })
+    },
+    out: function(req, res){
+        var id = req.query.id;
+        Links.findOne({_id: id}).exec(function(err, link){
+            if (err){
+                res.send(500, {error: "DB Error"});
+            }
+            var newClicks = link.clicks + 1;
+            sails.log('Old: ' + link.clicks);
+            sails.log('New: ' + newClicks);
+            res.redirect(link.url);
+            Links.update({_id: id}).set({clicks: newClicks}).then(function(){
+                console.log("Done");
+            });
+        })
+    }
+};
+
---
+++ api/models/Links.js
@@ -0,0 +1,46 @@
+/**
+ * Links.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    title: {
+      type: "string"
+    },
+    url: {
+      type: "string"
+    },
+    votes: {
+      type: "number",
+      defaultsTo: 0
+    },
+    clicks: {
+      type: "number",
+      defaultsTo: 0
+    }
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+  datastore: 'mongodb'
+
+};
+
